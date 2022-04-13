a40853d948056ff9c8272794569d7ccc4e2ec96b
Anton Kachurin <anton.kachurin@t-systems.com>
2022-04-13 20:06:42 +0300

    Add leetcode 1526 solution

---
+++ leetcode/1526_test.go
@@ -0,0 +1,55 @@
+package leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+func minNumberOperations(target []int) int {
+	size := len(target)
+
+	count := 0
+	for {
+		subsets := 0
+		lastMoved := false
+		for i := 0; i < size; i++ {
+			v := target[i]
+			if v == 0 {
+				if lastMoved {
+					subsets++
+				}
+				lastMoved = false
+				continue
+			}
+			lastMoved = true
+			target[i] = v - 1
+		}
+		if lastMoved { // when the last element is moved, there is at least one subset
+			subsets++
+		}
+		if subsets == 0 { // no changes to the list
+			break
+		}
+		count += subsets
+		fmt.Printf("Moved to %v in %d steps\n", target, subsets)
+	}
+	fmt.Println()
+	return count
+}
+
+func TestMinNumber(t *testing.T) {
+	cases := map[int][]int{
+		3: {1, 2, 3, 2, 1},
+		4: {3, 1, 1, 2},
+		7: {3, 1, 5, 4, 2},
+	}
+
+	for expected, v := range cases {
+		t.Run(fmt.Sprintf("minimal-%d", expected), func(t *testing.T) {
+			count := minNumberOperations(v)
+			if count != expected {
+				t.Fatalf("expected %d operations, got %d", expected, count)
+			}
+		})
+	}
+}
