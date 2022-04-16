924d2eb0b9ce75af633f94f0ca91ccf914481357
Anton Kachurin <anton.kachurin@t-systems.com>
2022-04-16 20:12:40 +0300

    Optimize slice accesses for 1769

--- leetcode/1769_test.go
+++ leetcode/1769_test.go
@@ -7,23 +7,19 @@ import (
 func minOperations(boxes string) []int {
 	results := make([]int, len(boxes))
 	left := 0
-	left = int(boxes[0]) - '0'
-	for i := 1; i < len(boxes); i++ {
-		results[i] += results[i-1] + left
-		if boxes[i] == '1' {
-			left++
-		}
+	prevLeft := 0
+	for i := 0; i < len(boxes); i++ {
+		prevLeft += left
+		results[i] = prevLeft
+		left += int(boxes[i]) - '0'
 	}
 
-	lastIndex := len(boxes) - 1
-	right := int(boxes[lastIndex]) - '0'
+	right := int(boxes[len(boxes)-1]) - '0'
 	prevRight := 0
 	for i := len(boxes) - 2; i >= 0; i-- {
 		prevRight += right
 		results[i] += prevRight
-		if boxes[i] == '1' {
-			right++
-		}
+		right += int(boxes[i]) - '0'
 	}
 	return results
 }
